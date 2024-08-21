83264ca0b730b760616568b5ca5c186a776b096d
ian723 <ndegwaian241@gmail.com>
2024-08-21 11:39:50 +0300

    message

--- routes/rnr.js
+++ routes/rnr.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const rnrController = require('../controllers/rnrController');
+const rnrController = require("../controllers/rnrController");
 
 // Get all reviews and ratings
-router.get('/', rnrController.getAllReviews);
+router.get("/", rnrController.getAllReviews);
 
 // Get a review and rating by ID
-router.get('/:id', rnrController.getReviewById);
+router.get("/:id", rnrController.getReviewById);
 
 // Create a new review and rating
-router.post('/', rnrController.createReview);
+router.post("/", rnrController.createReview);
 
 // Update a review and rating by ID
-router.put('/:id', rnrController.updateReview);
+router.put("/:id", rnrController.updateReview);
 
 // Delete a review and rating by ID
-router.delete('/:id', rnrController.deleteReview);
+router.delete("/:id", rnrController.deleteReview);
 
 module.exports = router;
